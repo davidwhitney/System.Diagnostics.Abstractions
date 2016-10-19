08f8a3262aa982618d273f393f24ed8880f438a4
David Whitney <david@davidwhitney.co.uk>
2016-10-19 18:05:14 +0100

    AssemblyInfo defaults.

--- System.Diagnostics.Abstractions/Properties/AssemblyInfo.cs
+++ System.Diagnostics.Abstractions/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("System.Diagnostics.Abstractions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("System.Diagnostics.Abstractions wrappers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ElectricHeadSoftware")]
 [assembly: AssemblyProduct("System.Diagnostics.Abstractions")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTrademark("N/A")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
